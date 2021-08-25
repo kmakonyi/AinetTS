6dd64ed607a4ef9b9f0bd821ab31718b367f4807
David <davidmenhur@gmail.com>
2021-08-25 16:37:24 +0200

    Include appdirs in the requirements

--- assist/setup.py
+++ assist/setup.py
@@ -4,6 +4,6 @@ setup(name='assist',
       version='0.1',
       package_dir={"": "src"},
       packages=find_packages('src'),
-      install_requires=['pandas>1.3'],
+      install_requires=['pandas>1.3, appdirs'],
       test_requires=['pytest']
       )
